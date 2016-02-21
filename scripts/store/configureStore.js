7e04cec226ba5ec24528142370e9f403ac757564
Michal Vlcek <mychalvlcek@gmail.com>
2016-02-21 09:40:44 +0100

    added redux-logger

--- scripts/store/configureStore.js
+++ scripts/store/configureStore.js
@@ -1,8 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
+import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const logger = createLogger({collapsed: true});
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware, logger)(createStore);
 
 export default function configureStore(initialState) {
     const store = createStoreWithMiddleware(rootReducer, initialState);
